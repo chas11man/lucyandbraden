c79d7d18efed742a378850ac053d5c949b0ac183
Charlie Marshall <chas11man@gmail.com>
2016-06-12 04:00:06 -0400

    settings

--- lucyandbraden/wsgi.py
+++ lucyandbraden/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lucyandbraden.settings")
-
-application = get_wsgi_application()
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
