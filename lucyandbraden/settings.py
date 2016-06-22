9d71b6883be1346da2f5f1f2f50b3835062201aa
Charlie Marshall <chas11man@gmail.com>
2016-06-22 18:13:54 -0400

    trying whitenoise

--- lucyandbraden/settings.py
+++ lucyandbraden/settings.py
@@ -124,5 +124,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'home', 'static'),
 )
