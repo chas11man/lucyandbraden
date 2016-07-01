3ae430eae2f40a0f2f64ef59d689d66989f0d68c
Charlie Marshall <chas11man@gmail.com>
2016-06-30 20:47:23 -0400

    jquery added, tabs added

---
+++ home/static/home/script.js
@@ -0,0 +1,17 @@
+function showPage(event, pageName) {
+
+    var i, pageContents, pageLinks
+
+    pageContents = document.getElementsByClassName("pageContent");
+    for(i = 0; i < pageContents.length; i++) {
+        pageContents[i].style.display = "none";
+    }
+
+    pageLinks = document.getElementsByClassName("pageLink");
+    for(i = 0; i < pageLinks.length; i++) {
+        pageLinks[i].className = pageLinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(pageName).style.display = "block";
+    event.currentTarget.className += " active";
+}
