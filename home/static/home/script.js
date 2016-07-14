02a466eda13baf7106112e7d5c3f9514e417d09e
Charlie Marshall <chas11man@gmail.com>
2016-07-14 17:41:03 -0400

    added google map

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -3,15 +3,18 @@ function showPage(event, pageName) {
     var i, pageContents, pageLinks
 
     pageContents = document.getElementsByClassName("pageContent");
-    for(i = 0; i < pageContents.length; i++) {
+    for (i = 0; i < pageContents.length; i++) {
         pageContents[i].style.display = "none";
     }
 
     pageLinks = document.getElementsByClassName("pageLink");
-    for(i = 0; i < pageLinks.length; i++) {
+    for (i = 0; i < pageLinks.length; i++) {
         pageLinks[i].className = pageLinks[i].className.replace(" active", "");
     }
 
     document.getElementById(pageName).style.display = "block";
     event.currentTarget.className += " active";
+
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(mapCenter);
 }
